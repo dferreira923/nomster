3d02f93a3359aad9a1de2daf884ea9a4ad546049
Doug <doug@pawdl.com>
2014-08-08 00:19:49 +0000

    Lesson 32

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,7 +20,8 @@ class PlacesController < ApplicationController
   end
 
   def show
-     @place = Place.find(params[:id])
+      @place = Place.find(params[:id])
+      @comment = Comment.new
   end
 
   def edit
